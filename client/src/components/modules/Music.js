f87c8b25443b110932872e1f9cb691ee30da4fc5
Akshaj Kadaveru <akshajk@mit.edu>
2020-05-26 23:23:42 -0400

    Yay

--- client/src/components/modules/Music.js
+++ client/src/components/modules/Music.js
@@ -48,7 +48,7 @@ class Music extends React.Component {
   
     componentDidUpdate(prevProps) {
       if(prevProps.url !== this.props.url) {
-        this.state.audio.src = this.props.url;
+        this.state.audio.src = this.props.url + "?cb=" + new Date().getTime();
         
       }
     }
