0b06896198d33c263071412e69afeb223926fd2f
Akshaj Kadaveru <akshajk@mit.edu>
2020-06-02 21:44:15 -0400

    Yay clearning out inactives woo hoo

--- server/api.js
+++ server/api.js
@@ -63,7 +63,7 @@ let clearOutInactives = () => {
   console.log("Clearing out inactives")
   Room.find({}).then((rooms) => {
     rooms.forEach((room) => {
-      if((room.status !== "waiting") && (room.status !== "roundFinished")) return;
+      
       let toRemove = []
       let counter = 0
       room.data.forEach((entry) => {
